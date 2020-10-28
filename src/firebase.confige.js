fa2ada635abce59559ce36e5123149198ee4b07d
amirhossain <amirhossaain2@gmail.com>
2020-10-28 18:12:25 +0600

    first commit

---
+++ src/firebase.confige.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBFKYybB6qT55oZzUFSpjhx7ZsrgrOaN-E",
+    authDomain: "loog-in.firebaseapp.com",
+    databaseURL: "https://loog-in.firebaseio.com",
+    projectId: "loog-in",
+    storageBucket: "loog-in.appspot.com",
+    messagingSenderId: "321918539772",
+    appId: "1:321918539772:web:5b3fff72681c8c14084479"
+  };
+
+  export default firebaseConfig;
